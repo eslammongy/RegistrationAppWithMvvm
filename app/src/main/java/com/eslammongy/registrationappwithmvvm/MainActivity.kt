f9a8a0a6eb9f5ed1341bb1cd1aa32863c54b5504
eslam <eslammongy995@gmail.com>
2021-07-06 13:49:06 +0200

    fix main activity issue

--- app/src/main/java/com/eslammongy/registrationappwithmvvm/MainActivity.kt
+++ app/src/main/java/com/eslammongy/registrationappwithmvvm/MainActivity.kt
@@ -1,27 +1,23 @@
 package com.eslammongy.registrationappwithmvvm
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
 import com.eslammongy.registrationappwithmvvm.data.UserPreferences
 import com.eslammongy.registrationappwithmvvm.ui.home.HomeActivity
 import com.eslammongy.registrationappwithmvvm.ui.startNewActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val userPreferences = UserPreferences(this)
-        userPreferences.authToken.asLiveData().observe(this , Observer {
-            val activityShouldBeStart = if (it == null) AuthActivity::class.java else HomeActivity::class.java
-            Handler(Looper.getMainLooper()).postDelayed({ startNewActivity(activityShouldBeStart) } , 2000)
-
+     userPreferences.accessToken.asLiveData().observe(this, Observer {
+            val activity = if (it == null) AuthActivity::class.java else HomeActivity::class.java
+            startNewActivity(activity)
         })
     }
 }
